f03af0b7ba714b2f82733688e63be116aeb1dfad
VitaliyLambov <vitaliy.lambov@mail.ru>
2018-05-24 15:57:54 +0300

    login form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,41 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CarsFormComponent } from './cars-form/cars-form.component';
 import { CarComponent } from './car/car.component';
+import { HeaderComponent } from './header/header.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { FooterComponent } from './footer/footer.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthguardGuard } from './authguard.guard';
+import { UserService } from './user.service';
 
+const appRoutes:Routes = [
+  {
+    path: '',
+    component: LoginFormComponent
+  },
+  {
+    path: 'dashboard',
+    canActivate: [AuthguardGuard],
+    component: DashboardComponent
+  }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     CarsFormComponent,
-    CarComponent
+    CarComponent,
+    HeaderComponent,
+    LoginFormComponent,
+    FooterComponent,
+    DashboardComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule
   ],
-  providers: [],
+  providers: [UserService, AuthguardGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
